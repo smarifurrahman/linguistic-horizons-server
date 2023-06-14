35ab126d97bf45fd66b75455204b8a097b1175ad
S M Arifur Rahman <smarifurrahman82@gmail.com>
2023-06-14 23:21:40 +0600

    users data get api

--- index.js
+++ index.js
@@ -32,11 +32,16 @@ async function run() {
         const classesCollection = client.db('linguisticHorizons').collection('classes');
 
         // users
+        app.get('/users', async (req, res) => {
+            const result = await usersCollection.find().toArray();
+            res.send(result);
+        })
+
         app.post('/users', async (req, res) => {
             const user = req.body;
             const query = { email: user.email }
             const existingUser = await usersCollection.findOne(query);
-            
+
             if (existingUser) {
                 return res.send({ message: 'user already exists' })
             }
