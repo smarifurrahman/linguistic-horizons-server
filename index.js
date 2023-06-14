b72269ee8a1775f4631abcca0e4586f2a9417188
S M Arifur Rahman <smarifurrahman82@gmail.com>
2023-06-14 22:14:19 +0600

    filted exited user while insert to db

--- index.js
+++ index.js
@@ -34,6 +34,13 @@ async function run() {
         // users
         app.post('/users', async (req, res) => {
             const user = req.body;
+            const query = { email: user.email }
+            const existingUser = await usersCollection.findOne(query);
+            
+            if (existingUser) {
+                return res.send({ message: 'user already exists' })
+            }
+
             const result = await usersCollection.insertOne(user);
             res.send(result);
         })
